5cc3a54b33ae364d240805e316dcf094c92aac3c
justinmandac <justinmandac@gmail.com>
2017-08-16 21:07:26 +0800

    Initial commit for React migration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const config = {
+  devtool : 'source-map',
+  entry : './src/main.js',
+  output: {
+    path : __dirname,
+    filename: 'app.bundle.js',
+  },
+  module : {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+      }
+    ],
+  },
+  resolve: {
+    extensions : ['.js', '.jsx', ]
+  },
+};
+
+module.exports = config;
